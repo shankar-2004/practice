9bf67f7c22a6e99687437e86e8a7075bbdc7aa22
shankaran.m <shankaran.m@zohocorp.com>
2025-10-06 22:09:13 +0530

    third message

--- myanother/server.js
+++ myanother/server.js
@@ -81,7 +81,7 @@ app.put("/api/users/:id",(req,res)=>{
     mockUsers[findUserIndex] = {
         id:parsedId, ...body
     }
-    return res.sendStatus(200); ss
+    return res.sendStatus(200); 
 });
 app.post("/api/users",(request,response) =>{
     console.log(request.body);
